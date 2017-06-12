d4f18396791c7760e9002f4a87bd0802c10f8281
Ezekiel Abuhoff <ezekielabuhoff@Ezekiels-MacBook-Pro.local>
2017-06-12 10:44:49 -0700

    created project and workspace files

---
+++ SidePiece/SidePiece.h
@@ -0,0 +1,19 @@
+//
+//  SidePiece.h
+//  SidePiece
+//
+//  Created by Ezekiel Abuhoff on 6/12/17.
+//  Copyright © 2017 Ezekiel Abuhoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SidePiece.
+FOUNDATION_EXPORT double SidePieceVersionNumber;
+
+//! Project version string for SidePiece.
+FOUNDATION_EXPORT const unsigned char SidePieceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SidePiece/PublicHeader.h>
+
+
